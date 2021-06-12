6f987bbae9c718eed5f0c3fd2bd72ca26e935056
halilturkoglu <halilturkoglucs@gmail.com>
2021-06-12 18:11:37 +0200

    Angular 12 ngrx redux store csrb csrb csrb csrb csrb csrb csrb

--- src/app/shopping/shopping-routing.module.ts
+++ src/app/shopping/shopping-routing.module.ts
@@ -1,15 +1,14 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
-import {AuthGuard} from "../auth-guard.service";
 import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
 
-const serversRoutes: Routes = [ // /servers prefix will be added by the app-routing.module.ts lazily
+const shoppingRoutes: Routes = [ // /servers prefix will be added by the app-routing.module.ts lazily
   {path: '', component: ShoppingListComponent}
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forChild(serversRoutes)
+    RouterModule.forChild(shoppingRoutes)
   ],
   exports: [
     RouterModule
